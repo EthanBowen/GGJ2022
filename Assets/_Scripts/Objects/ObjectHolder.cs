6141cdaf4028e9ae4b75c2719d1bcfd9eb57aac1
Ethan Bowen <Ethankbowen@gmail.com>
2022-01-29 13:25:24 -0700

    Ignore log files on Git and Placeable is almost done

---
+++ Assets/_Scripts/Objects/ObjectHolder.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHolder : MonoBehaviour
+{
+    public string[] objectTags;
+
+    public Transform objPosition;
+
+    [HideInInspector]
+    public GameObject heldObj = null;
+
+    [HideInInspector]
+    public bool readyToHold = true;
+
+    private Transform heldObjOriginalParent;
+
+    void Start()
+    {
+        if (objPosition == null)
+            objPosition = transform;
+
+        if (heldObj != null)
+            readyToHold = false;
+    }
+
+    public void HoldObject(Placeable other)
+    {
+        if (readyToHold)
+        {
+            readyToHold = false;
+            heldObj = other.gameObject;
+
+            heldObj.transform.position = objPosition.position;
+            heldObj.transform.rotation = objPosition.rotation;
+
+            heldObjOriginalParent = objPosition.parent;
+            heldObj.transform.parent = objPosition;
+
+            other.placed = true;
+        }
+    }
+
+    // When release comes from placeable
+    public void StopHolding()
+    {
+        if(heldObj)
+            heldObj.transform.parent = heldObjOriginalParent;
+        heldObjOriginalParent = null;
+        if (heldObj == null)
+            readyToHold = true;
+    }
+
+    public bool CanHold(string name)
+    {
+        foreach(string test in objectTags)
+        {
+            if(test.Equals(name))
+                return true;
+        }
+
+        return false;
+    }
+
+    // When release comes from ObjectHolder
+    public void ReleaseItem()
+    {
+        //if (heldObj)
+            //heldObj.GetComponent<Placeable>().RemoveFromHolder();
+    }
+}
---
+++ Assets/_Scripts/Objects/Placeable.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(XRGrabInteractable))]
+public class Placeable : MonoBehaviour
+{
+    public string objectTag = "";
+
+    [HideInInspector]
+    public Transform holder;
+    [HideInInspector]
+    public ObjectHolder objectHolder;
+    private ObjectHolder potentialHolder;
+
+    [HideInInspector]
+    public bool placed = false;
+    [HideInInspector]
+    public XRGrabInteractable interactable;
+
+    private bool readyToBeHeld = true;
+
+    private Transform initialParent;
+
+    private void Awake()
+    {
+        readyToBeHeld = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        interactable = GetComponent<XRGrabInteractable>();
+        initialParent = transform.parent;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    CheckGrab();
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (readyToBeHeld)
+        {
+            ObjectHolder temp = other.GetComponentInParent<ObjectHolder>();
+            if (temp != null && temp.readyToHold && temp.CanHold(objectTag))
+                potentialHolder = temp;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.Equals(potentialHolder.gameObject))
+        {
+            potentialHolder = null;
+        }
+    }
+
+    public void TryPlace()
+    {
+        bool output = false;
+
+        // If the Trigger is for a holder check its tag and make it hold the object
+        if (potentialHolder != null && potentialHolder.CanHold(objectTag) && potentialHolder.readyToHold)
+        {
+
+            Rigidbody rig = GetComponent<Rigidbody>();
+            rig.constraints = RigidbodyConstraints.FreezeAll;
+
+            holder = potentialHolder.objPosition;
+            objectHolder = potentialHolder;
+
+            objectHolder.HoldObject(this);
+
+            output = true;
+        }
+
+        readyToBeHeld = !output;
+    }
+
+    public bool FreeHolder(GameObject other)
+    {
+        ObjectHolder temp = other.GetComponentInParent<ObjectHolder>();
+        // Collision is a holder that isn't holding an object
+        if (temp != null)
+        {
+            if (temp.heldObj == null && !temp.readyToHold)
+            {
+                temp.StopHolding();
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Remove from a holder if it is attached to one
+    public void Grabbed()
+    {
+        if (placed && holder != null)// && interactable.attachedToHand)
+        {
+            Rigidbody rig = GetComponent<Rigidbody>();
+            rig.constraints = RigidbodyConstraints.None;
+            
+            placed = false;
+            transform.parent = initialParent;
+            objectHolder.heldObj = null;
+            holder = null;
+        }
+    }
+
+    // Called from ObjectHolder
+    public void AttemptRemoveFromHolder()
+    {
+        Rigidbody rig = GetComponent<Rigidbody>();
+        rig.constraints = RigidbodyConstraints.None;
+
+        placed = false;
+        transform.parent = initialParent;
+        objectHolder.heldObj = null;
+
+        holder = null;
+    }
+}
