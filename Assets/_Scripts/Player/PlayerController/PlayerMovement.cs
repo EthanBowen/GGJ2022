ef432260021fc7de55ba346131026dae8808d814
Ethan Bowen <Ethankbowen@gmail.com>
2022-01-29 02:57:45 -0700

    Movement feels pretty clean

--- Assets/_Scripts/Player/PlayerController/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerController/PlayerMovement.cs
@@ -1,3 +1,9 @@
+/* 
+ * 
+ * 
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,9 +17,14 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController CC;
 
     [SerializeField]
-    private Transform HeadPos;
+    private Transform HeadTransform;
 
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 1.0f;
+
+    public float gravity = -9.81f;
+    public float fallingSpeed = 0f;
+
+    public LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Start()
@@ -36,9 +47,38 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 vector = HeadPos.position;
-        CC.center = new Vector3(vector.x, vector.y / 2, vector.z);
+        CapsuleAdjustToHeadset();
 
+        Vector2 moveVector;
+        MovementController.TryGetFeatureValue(CommonUsages.primary2DAxis, out moveVector);
 
+        Quaternion headYaw = Quaternion.Euler(0, HeadTransform.rotation.eulerAngles.y, 0);
+
+        if(CheckIfGrounded())
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.deltaTime;
+        }
+        
+        CC.Move(headYaw * new Vector3(moveVector.x, fallingSpeed, moveVector.y) * movementSpeed * Time.deltaTime);
+    }
+
+    private void CapsuleAdjustToHeadset()
+    {
+        Vector3 headPos = HeadTransform.localPosition;
+        CC.center = new Vector3(headPos.x, headPos.y / 2, headPos.z);
+        CC.height = headPos.y;
+    }
+
+    // Uses Spherecast to determine if player is in the ground
+    private bool CheckIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(CC.center);
+        float rayLength = CC.center.y + 0.01f;
+        bool hasHit = Physics.SphereCast(rayStart, CC.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
     }
 }
